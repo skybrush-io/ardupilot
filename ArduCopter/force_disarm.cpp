366400b582b114845844de17b03b5dc6588260e5
Tamas Nepusz <ntamas@gmail.com>
2025-04-13 12:17:46 +0200

    feat: added force-disarm option for EKF failsafe

--- ArduCopter/force_disarm.cpp
+++ ArduCopter/force_disarm.cpp
@@ -7,8 +7,9 @@ void Copter::force_disarm_without_questions(const AP_Arming::Method method)
     // Try to disarm the motors forcibly via the AP_Arming module
     if (!AP::arming().disarm(method, /* do_disarm_checks = */ false)) {
         // AP_Arming module refused to disarm. There must be a reason for this,
-        // but the hard fence overrides everything, so we talk directly to the
-        // motors instead
+        // but this function is meant for cases when we do not want to be
+        // overridden at all, so we talk directly to the motors instead
         motors->armed(false);
+        motors->output();
     }
 }
