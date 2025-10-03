a1c82df26d149efc2d123bc884837208d2e7e335
alexshidagoatnocap <115957532+alexshidagoatnocap@users.noreply.github.com>
2025-10-03 16:53:44 -0500

    removed macOS specific linker flags in board.py

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -762,7 +762,7 @@ class sitl(Board):
         cfg.check_librt(env)
         cfg.check_feenableexcept()
 
-        env.LINKFLAGS += ['-pthread', '-Wl,--gc-sections']
+        env.LINKFLAGS += ['-pthread',]
 
         if cfg.env.DEBUG and 'clang++' in cfg.env.COMPILER_CXX and cfg.options.asan:
              env.LINKFLAGS += ['-fsanitize=address']
