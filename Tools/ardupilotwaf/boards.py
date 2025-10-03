a760f758e5afe1cbc82ecf51b01b1d2cd1e83a0e
alexshidagoatnocap <115957532+alexshidagoatnocap@users.noreply.github.com>
2025-10-03 16:19:03 -0500

    removed -ld_classic flag only used on macOS for the default LINKFLAGS

--- Tools/ardupilotwaf/boards.py
+++ Tools/ardupilotwaf/boards.py
@@ -762,7 +762,7 @@ class sitl(Board):
         cfg.check_librt(env)
         cfg.check_feenableexcept()
 
-        env.LINKFLAGS += ['-pthread', '-Wl,--gc-sections,-ld_classic']
+        env.LINKFLAGS += ['-pthread', '-Wl', '--gc-sections', '-Wl']
 
         if cfg.env.DEBUG and 'clang++' in cfg.env.COMPILER_CXX and cfg.options.asan:
              env.LINKFLAGS += ['-fsanitize=address']
