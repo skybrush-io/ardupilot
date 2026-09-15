f3c5517a6aa13646132552628ebc06a9d18d0165
Tamas Nepusz <ntamas@gmail.com>
2026-09-15 15:09:17 +0200

    fix: duration of trajectory in first scene must be derived from the landing time, not from the full duration

--- libraries/AC_DroneShowManager/AC_DroneShowManager_Loader.cpp
+++ libraries/AC_DroneShowManager/AC_DroneShowManager_Loader.cpp
@@ -213,7 +213,32 @@ bool AC_DroneShowManager::_load_show_file_from_storage()
             sb_trajectory_t* trajectory = scene ? sb_screenplay_scene_get_trajectory(scene) : nullptr;
             
             if (trajectory) {
-                sb_screenplay_scene_set_duration_msec(scene, _trajectory_stats.duration_msec);
+                if (i == 0) {
+                    // Special case: first scene is the main show scene so we need to
+                    // set the trajectory duration so that it matches the time when we
+                    // need to switch from our control to ArduPilot's landing algorithm.
+                    // 
+                    // If the trajectory is not circular _or_ we are not correcting the
+                    // end of the trajectory based on actual placement, then we can
+                    // switch over at landing_time (from _trajectory_stats). However,
+                    // _if_ we are correcting the end of the trajectory, then we need to
+                    // switch over at roughly halfway through the descent from
+                    // SHOW_TAKEOFF_ALT because the correction is designed to take place
+                    // in the first half.
+                    // 
+                    // However, since we don't know whether there will be a correction
+                    // at takeoff or not (SHOW_OPTIONS can change between the time when
+                    // the show is loaded and the time when the show starts), the
+                    // easiest is simply to hand control over at halfway between
+                    // landing_time and the full duration of the landing trajectory.
+                    sb_screenplay_scene_set_duration_msec(
+                        scene,
+                            (_trajectory_stats.landing_time_sec * 1000.0f +
+                             _trajectory_stats.duration_msec) / 2.0f
+                    );
+                } else {
+                    sb_screenplay_scene_set_duration_msec(scene, _trajectory_stats.duration_msec);
+                }
             }
 
             if (event_list && _params.pyro_spec.time_compensation_msec != 0)
